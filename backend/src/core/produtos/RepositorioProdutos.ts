ae6adbc226a26b25a292a3d3f3d1aa25336b2429
Fabinho Flauzino <FabioFlauzino@hotmail.com>
2025-06-05 00:34:19 -0300

    Iniciando projeto e criando Rota para obter produtos

---
+++ backend/src/core/produtos/RepositorioProdutos.ts
@@ -0,0 +1,15 @@
+import Produto from "./Produto";
+
+export default class RepositorioProdutos {
+  itens: Produto[] = [
+    new Produto("Colher", 5.99),
+    new Produto("Mouse", 39.99),
+    new Produto("Teclado", 99.99),
+  ]
+
+  obterTodos() {
+    return this.itens
+  }
+
+  
+}
